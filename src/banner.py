bc34f834fab5f75e3df9c6caef679b4e18ff2635
Mohsin Mukhtiar Lashari <160683327+president-xd@users.noreply.github.com>
2024-08-27 11:19:55 +0500

    Create banner.py

---
+++ src/banner.py
@@ -0,0 +1,27 @@
+def banner():
+  print('''
+     SSSSSSSSSSSSSSS                                                                                     
+ SS:::::::::::::::S                                                                                    
+S:::::SSSSSS::::::S                                                                                    
+S:::::S     SSSSSSS                                                                                    
+S:::::S           ppppp   ppppppppp     aaaaaaaaaaaaa      mmmmmmm    mmmmmmm yyyyyyy           yyyyyyy
+S:::::S           p::::ppp:::::::::p    a::::::::::::a   mm:::::::m  m:::::::mmy:::::y         y:::::y 
+ S::::SSSS        p:::::::::::::::::p   aaaaaaaaa:::::a m::::::::::mm::::::::::my:::::y       y:::::y  
+  SS::::::SSSSS   pp::::::ppppp::::::p           a::::a m::::::::::::::::::::::m y:::::y     y:::::y   
+    SSS::::::::SS  p:::::p     p:::::p    aaaaaaa:::::a m:::::mmm::::::mmm:::::m  y:::::y   y:::::y    
+       SSSSSS::::S p:::::p     p:::::p  aa::::::::::::a m::::m   m::::m   m::::m   y:::::y y:::::y     
+            S:::::Sp:::::p     p:::::p a::::aaaa::::::a m::::m   m::::m   m::::m    y:::::y:::::y      
+            S:::::Sp:::::p    p::::::pa::::a    a:::::a m::::m   m::::m   m::::m     y:::::::::y       
+SSSSSSS     S:::::Sp:::::ppppp:::::::pa::::a    a:::::a m::::m   m::::m   m::::m      y:::::::y        
+S::::::SSSSSS:::::Sp::::::::::::::::p a:::::aaaa::::::a m::::m   m::::m   m::::m       y:::::y         
+S:::::::::::::::SS p::::::::::::::pp   a::::::::::aa:::am::::m   m::::m   m::::m      y:::::y          
+ SSSSSSSSSSSSSSS   p::::::pppppppp      aaaaaaaaaa  aaaammmmmm   mmmmmm   mmmmmm     y:::::y           
+                   p:::::p                                                          y:::::y            
+                   p:::::p                                                         y:::::y             
+                  p:::::::p                                                       y:::::y              
+                  p:::::::p                                                      y:::::y               
+                  p:::::::p                                                     yyyyyyy                
+                  ppppppppp                                                                            
+                                                                                                       
+                                                                                            by President-xd
+  ''')
